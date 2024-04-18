9f5b3b2f8c6ba6d15b7fdb2a842c7b2dff04e039
Angel Yesid Duque <angelyesidducr@ufps.edu.co>
2024-04-18 10:15:01 -0500

    cambios endpoint pqrs

--- pqrs/src/main/java/BackendSiadseUfps/siadse/controller/PQRSAdminController.java
+++ pqrs/src/main/java/BackendSiadseUfps/siadse/controller/PQRSAdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/pqrs")
+@RequestMapping("/public/pqrs")
 public class PQRSAdminController {
 
     @Autowired
