497cdbc89094942f20b3ba1d5c72460333204adb
JulianRivers <juliancamilorifo@ufps.edu.co>
2024-05-02 16:29:04 -0500

    correcciones, properties, data sql

--- src/main/java/BackendSiadseUfps/siadse/service/interfaces/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package BackendSiadseUfps.siadse.service.interfaces;
-
-import BackendSiadseUfps.siadse.entity.Usuario;
-
-public interface UsuarioService {
-	Usuario guardarUsuario(Usuario usuario);
-    Usuario obtenerUsuarioPorId(Long id);
-    Usuario obtenerUsuarioPorEmail(String email);
-}
