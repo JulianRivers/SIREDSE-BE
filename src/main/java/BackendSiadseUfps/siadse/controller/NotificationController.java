1e21ef590d9ca5d460108c69999897e36b405d82
Dgonzalezseek <162947296+Dgonzalezseek@users.noreply.github.com>
2024-05-29 12:47:28 -0500

    rama abi

--- src/main/java/BackendSiadseUfps/siadse/controller/NotificationController.java
+++ src/main/java/BackendSiadseUfps/siadse/controller/NotificationController.java
@@ -1,7 +1,6 @@
 package BackendSiadseUfps.siadse.controller;
 
 import BackendSiadseUfps.siadse.service.implementations.EmailService;
-import BackendSiadseUfps.siadse.service.implementations.SMSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +11,9 @@ public class NotificationController {
     @Autowired
     private EmailService emailService;
 
-    @Autowired
-    private SMSService smsService;
-
     @PostMapping("/email")
     public void sendEmailNotification(@RequestParam String to, @RequestParam String subject, @RequestParam String body) {
         emailService.sendEmail(to, subject, body);
     }
 
-    @PostMapping("/sms")
-    public void sendSMSNotification(@RequestParam String to, @RequestParam String body) {
-        smsService.sendSMS(to, body);
-    }
 }
