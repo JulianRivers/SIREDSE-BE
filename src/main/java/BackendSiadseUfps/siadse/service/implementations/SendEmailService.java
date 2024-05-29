c9f3ef121e6295373ecc610452d0fbc538493749
Dgonzalezseek <162947296+Dgonzalezseek@users.noreply.github.com>
2024-05-29 16:46:24 -0500

    rama abi

--- src/main/java/BackendSiadseUfps/siadse/service/implementations/SendEmailService.java
+++ src/main/java/BackendSiadseUfps/siadse/service/implementations/SendEmailService.java
@@ -6,7 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailService {
+public class SendEmailService {
 
     @Autowired
     private JavaMailSender mailSender;
